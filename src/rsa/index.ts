4a241ac536a5b1ab27cdeaf0daec331c82c76526
taisan <131413638+taisan11@users.noreply.github.com>
2024-05-13 19:09:20 +0900

    update 型

--- src/rsa/index.ts
+++ src/rsa/index.ts
@@ -1,4 +1,4 @@
-export function genKeyRss() {
+export function genKeyRss(): Promise<CryptoKeyPair> {
     return crypto.subtle.generateKey(
         {
             name: "RSA-PSS",
@@ -8,17 +8,21 @@ export function genKeyRss() {
         },
         false, //whether the key is extractable (i.e. can be used in exportKey)
         ["sign", "verify"] //can be any combination of "sign" and "verify"
-    )
+    ) as Promise<CryptoKeyPair>;
 }
-export function exportKeyRss(key: CryptoKey) {
-    return crypto.subtle.exportKey("jwk", key)
+
+export function exportKeyRss(key: CryptoKey): Promise<JsonWebKey> {
+    return crypto.subtle.exportKey("jwk", key) as Promise<JsonWebKey>;
 }
-export function importKeyRss(key: JsonWebKey) {
-    return crypto.subtle.importKey("jwk", key, {name: "RSA-PSS", hash: {name: "SHA-256"}}, true, ["verify"])
+
+export function importKeyRss(key: JsonWebKey): Promise<CryptoKey> {
+    return crypto.subtle.importKey("jwk", key, {name: "RSA-PSS", hash: {name: "SHA-256"}}, true, ["verify"]) as Promise<CryptoKey>;
 }
-export function signRss(key: CryptoKey, data: ArrayBuffer) {
-    return crypto.subtle.sign("RSA-PSS", key, data)
+
+export function signRss(key: CryptoKey, data: ArrayBuffer): Promise<ArrayBuffer> {
+    return crypto.subtle.sign("RSA-PSS", key, data) as Promise<ArrayBuffer>;
 }
-export function verifyRss(key: CryptoKey, signature: ArrayBuffer, data: ArrayBuffer) {
-    return crypto.subtle.verify("RSA-PSS", key, signature, data)
+
+export function verifyRss(key: CryptoKey, signature: ArrayBuffer, data: ArrayBuffer): Promise<boolean> {
+    return crypto.subtle.verify("RSA-PSS", key, signature, data) as Promise<boolean>;
 }
